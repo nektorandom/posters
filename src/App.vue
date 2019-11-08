20933fa0421bee9dadb30a52d8c20b410e2ef4d7
Posivnich <lposivnich@code-pilots.ru>
2019-11-08 18:43:11 +0300

    upd css, component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <SearchBlock msg="Search"/>
+    <SearchBlock/>
   </div>
 </template>
 
--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,16 +1,14 @@
 <template>
   <div class="hello">
 
-    <section class='PosterSearch'>
+    <section class='poster-search'>
       <header class='header'>
         <h1>{{ title }}</h1>
         <h3>Find your favourite movie posters.</h3>
       </header>
       <main>
         <p>
-          <label class='label' for='movie-name'>
-            Movie title:
-          </label>
+          <label class='label' for='movie-name'></label>
           <input
                   class='searchBox'
                   type='search'
@@ -21,14 +19,14 @@
                   :value="movieName"
                   @input="updateSearch"
           />
-          <button id='search-button' class='searchButton' :disabled="disableState" @click="handleClick">
+          <button id='search-button' class='rd-btn btn-search' :disabled="disableState" @click="handleClick">
             Search
           </button>
           <br />
         </p>
         <p id='msg'>{{ msg }}</p>
       </main>
-      <section id='poster-grid' class='PosterGrid'>
+      <section id='poster-grid' class='poster-grid'>
         <img v-for="movie in posters" :key="movie.Title" :src="movie.Poster" :alt="movie.Title">
       </section>
     </section>
@@ -71,7 +69,8 @@ export default {
             )
                 .then(resp => resp.json())
                 .then(result => {
-                    this.setPosters(result.Search)
+                    this.setPosters(result.Search);
+                    this.setMsg('');
                 })
                 .catch(error => {
                     throw error
@@ -82,6 +81,10 @@ export default {
 </script>
 
 <style scoped>
+  body {
+    font-size: 16px;
+  }
+
 h3 {
   margin: 40px 0 0;
 }
@@ -96,4 +99,74 @@ li {
 a {
   color: #42b983;
 }
+
+input {
+  font-size: 18px;
+  line-height: 40px;
+  height: 40px;
+  width: 400px;
+}
+
+.rd-btn {
+  cursor: pointer;
+  position: relative;
+  display: inline-block;
+  text-align: center;
+  text-decoration: none;
+  vertical-align: bottom;
+  white-space: nowrap;
+  color: #fff;
+  background: #42b983;
+  border: none;
+  box-sizing: border-box;
+  line-height: 40px;
+  height: 40px;
+  padding: 0 32px;
+  overflow: hidden;
+  opacity: 1;
+  transition: background-color .3s, opacity .3s;
+  font-size: 16px;
+
+  text-rendering: auto;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+}
+
+  .rd-btn:hover {
+     background: #45c28a;
+   }
+
+  .rd-btn:disabled {
+    background: #b4b4b4;
+  }
+
+  .btn-search {
+    margin-left: 10px;
+  }
+
+  .poster-search {
+    max-width: 1200px;
+    margin: 0 auto;
+  }
+
+  .poster-grid {
+    display: flex;
+    justify-content: flex-start;
+    flex-wrap: wrap;
+  }
+
+  .poster-grid  img {
+    width: 24%;
+    object-fit: cover;
+    margin-bottom: 10px;
+    margin-right: 16px;
+  }
+
+  .poster-grid  img:nth-of-type(4n) {
+    margin-right: 0;
+  }
 </style>
